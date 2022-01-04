a18fdcc3f89303a98a7ad21644642bded98c089e
budinugroho1397 <97114993+budinugroho1397@users.noreply.github.com>
2022-01-04 23:12:08 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,64 @@
+<!-- Sidebar -->
+    <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon">
+          <img src="img/logo/logo.png">
+        </div>
+        <div class="sidebar-brand-text mx-3">Elektronik</div>
+      </a>
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Beranda</span></a>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Data Master
+      </div>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="nama_barang.php" >
+          <i class="far fa-fw fa-user"></i>
+          <span>Nama Barang</span>
+        </a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link collapsed" href="pelanggan.php" >
+          <i class="far fa-fw fa-bell"></i>
+          <span>pelanggan</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="penyediaan.php" >
+          <i class="far fa-fw fa-bell"></i>
+          <span>Penyediaan</span>
+        </a>
+      </li>
+	  
+	  
+      
+         
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Transaksi
+      </div>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="transaksi.php" >
+          <i class="fas fa-fw fa-columns"></i>
+          <span>Data Transaksi</span>
+        </a>
+        
+      </li>
+	  
+	  <li class="nav-item">
+        <a class="nav-link collapsed" href="laporan.php" >
+          <i class="fas fa-fw fa-file"></i>
+          <span>laporan</span>
+        </a>
+        
+      </li>
+     
+      <hr class="sidebar-divider">
+      <div class="version" id="version-ruangadmin"></div>
+    </ul>
+    <!-- Sidebar -->
